326399d1499a536e26fe28dfd1f511fe89970f9a
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-25 21:26:57 -0500

    Add json of songs

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require("express");
+// import { playList } from './playList.js';
+
+const express = require('express');
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res)=>{
-  res.send("Hi server");
+app.get('/', (req, res) => {
+  res.send('Hi server');
 });
 
+app.get('/songs', (req, res) => {
+  res.json({
+    items:[
+      {
+        nameSong: 'Stan',
+        singer: 'Eminen & Dido',
+      },
+      {
+        nameSong: 'Empire State Of Mind',
+        singer: 'JAY-Z, Alicia Keys',
+      },
+      {
+        nameSong: 'Kangaroo Court',
+        singer: 'Capital Cities',
+      }
+    ]
+  });
+});
+
+// app.get('/playList', (req, res) => {
+//   res.send(playList);
+// });
+
 app.listen(port, () => {
-  console.log("Mi puerto es: " + port);
+  console.log('Mi puerto es: ' + port);
 });
