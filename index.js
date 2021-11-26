9cdd0b17e31deef9bb6261768c76316a9fc1b915
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-26 00:40:47 -0500

    Params and query in get

--- index.js
+++ index.js
@@ -12,22 +12,61 @@ app.get('/', (req, res) => {
 
 app.get('/songs', (req, res) => {
   res.json({
-    items:[
+    items: [
       {
+        id: 1,
         nameSong: 'Stan',
         singer: 'Eminen & Dido',
       },
       {
+        id: 2,
         nameSong: 'Empire State Of Mind',
         singer: 'JAY-Z, Alicia Keys',
       },
       {
+        id: 3,
         nameSong: 'Kangaroo Court',
         singer: 'Capital Cities',
-      }
-    ]
+      },
+    ],
   });
 });
+
+app.get("/products/food", (req, res) => {
+  res.send("Esta es un ruta especifica")
+})
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: `product${id}`
+  })
+})
+
+app.get("/users/:type", (req, res) => {
+  const { type } = req.params;
+  const { limit } = req.query;
+  let size = limit || 10
+  let array = []
+  for (let i = 0; i < size; i++) {
+    array.push({
+      name: `user${i}`,
+      type
+    })
+  }
+  res.json(array)
+})
+
+
+app.get('/categories/:categoriesId/products/:productId', (req, res) => {
+  const { categoriesId, productId } = req.params;
+  res.json({
+    categoriesId,
+    productId,
+  });
+});
+
 
 // app.get('/playList', (req, res) => {
 //   res.send(playList);
