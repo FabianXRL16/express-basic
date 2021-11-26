5a45d46b476ea9bf074e91bded5f8f9b393c6271
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-26 03:40:22 -0500

    Patch in products

--- routes/products.router.js
+++ routes/products.router.js
@@ -2,38 +2,48 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   const { limit } = req.query;
-  let size = limit || 10
+  let size = limit || 10;
   let products = [];
   for (let i = 0; i < size; i++) {
     products.push({
       id: `000${i}`,
       name: `product_${i}`,
-      type: "smatphone"
+      type: 'smatphone',
     });
   }
-  res.json(products)
-})
+  res.json(products);
+});
 
-router.get("/food", (req, res) => {
-  res.send("Esta es un ruta especifica")
-})
+router.get('/food', (req, res) => {
+  res.send('Esta es un ruta especifica');
+});
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
   res.json({
     id,
-    name: `product${id}`
-  })
-})
+    name: `product${id}`,
+  });
+});
 
 router.post('/', (req, res) => {
   const body = req.body;
   res.json({
-    message: "Product created",
-    data: body
-  })
-})
+    message: 'Product created',
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Update Product',
+    name: body,
+    id,
+  });
+});
 
 module.exports = router;
