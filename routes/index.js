b02a8a9b174d53794e792ea863502c2b00d42f44
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-27 17:47:21 -0500

    Main endpoint /api/v1 path global

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
+const express = require('express');
+
 const songsRouter = require('./songs.router');
 const routerProducts = require('./products.router');
 const routerCategories = require('./categories.router');
 const routerUsers = require('./users.router');
-const routerPlayList = require('./playList.router')
+const routerPlayList = require('./playList.router');
 
 function getApi(app) {
-  app.use('/songs', songsRouter);
-  app.use('/products', routerProducts);
-  app.use('/categories', routerCategories);
-  app.use('/users', routerUsers);
-  app.use('/playlist', routerPlayList)
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/songs', songsRouter);
+  router.use('/products', routerProducts);
+  router.use('/categories', routerCategories);
+  router.use('/users', routerUsers);
+  router.use('/playlist', routerPlayList);
 }
 
 module.exports = getApi;
